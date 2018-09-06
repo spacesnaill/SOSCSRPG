b70c92e29ab2d054e035074668de64258b6e2e9b
Patrick Geiger <patrickjgeiger@gmail.com>
2018-09-06 07:43:17 -0400

    added player class and its view model

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+     class GameSession
+     {
+          Player CurrentPlayer { get; set; }
+
+          public GameSession()
+          {
+               CurrentPlayer = new Player();
+               CurrentPlayer.Name = "Patrick";
+               CurrentPlayer.Gold = 1000000;
+          }
+     }
+}
