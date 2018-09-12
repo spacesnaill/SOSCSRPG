bb436d1359d58bd533de2795005473ee7a5d65ce
Patrick Geiger <patrickjgeiger@gmail.com>
2018-09-12 17:24:00 -0400

    implemented functionality for the movement buttons

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,16 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
 using Engine.Factories;
+using System.ComponentModel;
 
 namespace Engine.ViewModels
 {
-     public class GameSession
+     public class GameSession : INotifyPropertyChanged
      {
+
+          private Location _currentLocation;
           public World CurrentWorld { get; set; }
 
           public Player CurrentPlayer { get; set; }
 
-          public Location CurrentLocation { get; set; }
+          public Location CurrentLocation
+          {
+               get { return _currentLocation; }
+               set
+               {
+                    _currentLocation = value;
+                    OnPropertyChanged("CurrentLocation");
+                    OnPropertyChanged("HasLocationToNorth");
+                    OnPropertyChanged("HasLocationToSouth");
+                    OnPropertyChanged("HasLocationToEast");
+                    OnPropertyChanged("HasLocationToWest");
+               }
+          }
+
+          public bool HasLocationToNorth
+          {
+               get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null; }
+          }
+
+          public bool HasLocationToSouth
+          {
+               get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null; }
+          }
+
+          public bool HasLocationToEast
+          {
+               get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null; }
+          }
+
+          public bool HasLocationToWest
+          {
+               get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null; }
+          }
 
           public GameSession()
           {
@@ -32,6 +67,34 @@ namespace Engine.ViewModels
                CurrentWorld = factory.CreateWorld();
                CurrentLocation = CurrentWorld.LocationAt(0, 0);
                
+          }
+
+          public void MoveNorth()
+          {
+               CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+
+          }
+
+          public void MoveWest()
+          {
+               CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+          }
+
+          public void MoveEast()
+          {
+               CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+          }
+
+          public void MoveSouth()
+          {
+               CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+          }
+
+          public event PropertyChangedEventHandler PropertyChanged;
+
+          protected virtual void OnPropertyChanged(String propertyName)
+          {
+               PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
           }
      }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -31,5 +31,25 @@ namespace WPFUI
 
                DataContext = _gameSession; //what the xaml file will use for the values of labels and controls
           }
+
+          private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+          {
+               _gameSession.MoveNorth();
+          }
+
+          private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+          {
+               _gameSession.MoveWest();
+          }
+
+          private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+          {
+               _gameSession.MoveEast();
+          }
+
+          private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+          {
+               _gameSession.MoveSouth();
+          }
      }
 }
