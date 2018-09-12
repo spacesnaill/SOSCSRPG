6f999e744ffe046581f98c894c3349e9f8748f9f
Patrick Geiger <patrickjgeiger@gmail.com>
2018-09-12 15:54:39 -0400

    added world factory and a few locations

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+     internal class WorldFactory
+     {
+          internal World CreateWorld()
+          {
+               World newWorld = new World();
+
+               newWorld.AddLocation(0, -1, "Home", 
+                    "This is your home", 
+                    "/Engine;component/Images/Locations/Home.gif");
+
+               newWorld.AddLocation(-1, -1, "Farmer's House",
+                    "This is the house of your neighbor, Farmer Ted.",
+                    "/Engine;component/Images/Locations/Farmhouse.png");
+
+               newWorld.AddLocation(-2, -1, "Farmer's Field",
+                    "There are rows of corn growing here, with giant rats hiding between them.",
+                    "/Engine;component/Images/Locations/FarmFields.png");
+
+               return newWorld;
+          }
+     }
+}
