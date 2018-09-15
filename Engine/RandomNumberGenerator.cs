b2338207f67ffea774b38f0856898e9b4f1c2c3b
Patrick Geiger <patrickjgeiger@gmail.com>
2018-09-15 18:31:47 -0400

    added random number generator using a deterministic method for now as it is simpler and easy to understand

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+     public static class RandomNumberGenerator
+     {
+          private static readonly Random _simpleGenerator = new Random();
+          
+          public static int NumberBetween(int minimumValue, int maximumValue)
+          {
+               return _simpleGenerator.Next(minimumValue, maximumValue + 1);
+          }
+     }
+}
