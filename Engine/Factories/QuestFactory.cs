fd50de470fc7d98f54b8abbd1cef620e7f31d5c4
Patrick Geiger <patrickjgeiger@gmail.com>
2018-09-15 17:13:47 -0400

    added quest factory and necessary scaffolding for adding quests to the game

---
+++ Engine/Factories/QuestFactory.cs
@@ -0,0 +1,34 @@
+﻿using Engine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Engine.Factories
+{
+     internal static class QuestFactory
+     {
+          private static readonly List<Quest> _quests = new List<Quest>();
+
+          static QuestFactory()
+          {
+               //declare items needed to complete the quest and its reward items
+               List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+               List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+
+               itemsToComplete.Add(new ItemQuantity(9001, 5));
+               rewardItems.Add(new ItemQuantity(1002, 1));
+
+               //create the quest
+               _quests.Add(new Quest(1,
+                    "Clear the herb garden",
+                    "Defeat the snakes in the Herbalist's Garden",
+                    itemsToComplete,
+                    25, 10,
+                    rewardItems));
+          }
+
+          internal static Quest GetQuestByID(int id)
+          {
+               return _quests.FirstOrDefault(quest => quest.ID == id); //default is null
+          }
+     }
+}
